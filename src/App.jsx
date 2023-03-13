ddc575cd4b2ba4acc6ada6b43d8145dd96f16ac8
dilsoncampelo10 <dilsono316@gmail.com>
2023-03-13 17:47:08 -0300

    page login

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { Login } from './pages/login';
+
+
+function App() {
+  return (
+    <div>
+      <Login />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/login/index.jsx
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { Container } from '@mui/system';
+import { Button, Card, Grid } from '@mui/material';
+
+export function Login() {
+    return (
+
+        <Box
+            component="form"
+            sx={{
+                '& .MuiTextField-root': { m: 1, width: '38ch' },
+                display: 'flex',
+                justifyContent: 'center',
+                width: '100%'
+            }}
+            noValidate
+            autoComplete="off"
+        >
+            <Card sx={{paddingX:20,paddingBottom:10}}>
+                <Grid>
+                    <Grid item xs={12} sx={{ textAlign: 'center' }}>
+                        <h1>Login</h1>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                            label="CPF"
+                            id="outlined-size-small"
+                            placeholder="Insiea CPF"
+                            size="small"
+                        />
+
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                            label="Senha"
+                            id="outlined-size-small"
+                            placeholder='Insira senha'
+                            size="small"
+                            type="password"
+                        />
+
+                    </Grid>
+                    <Grid item>
+                        <Button variant="contained">Login</Button>
+
+                    </Grid>
+
+                </Grid>
+            </Card>
+        </Box>
+
+    );
+}
