4b0e02d061662eefab796d2701c85de34af35f21
dilsoncampelo10 <dilsono316@gmail.com>
2023-03-20 17:21:02 -0300

    finish login page

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -3,7 +3,7 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import { Container } from '@mui/system';
 import { Button, Card, Grid } from '@mui/material';
-
+import './css/style.css';
 export function Login() {
     return (
 
@@ -12,43 +12,47 @@ export function Login() {
             sx={{
                 '& .MuiTextField-root': { m: 1, width: '38ch' },
                 display: 'flex',
-                justifyContent: 'center',
+                justifyContent: 'space-between',
                 width: '100%'
             }}
             noValidate
             autoComplete="off"
         >
-            <Card sx={{paddingX:20,paddingBottom:10}}>
-                <Grid>
-                    <Grid item xs={12} sx={{ textAlign: 'center' }}>
-                        <h1>Login</h1>
+            <div className='image'>
+                
+            </div>
+            <div>
+                <Card sx={{ paddingX: 20, paddingY: 10, height:'100%' }}>
+                    <Grid>
+                        <Grid item xs={12} sx={{ textAlign: 'center' }}>
+                            <h1>Login</h1>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                label="CPF"
+                                id="outlined-size-small"
+                                placeholder="Insiea CPF"
+                                size="small"
+                                className='input'
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                label="Senha"
+                                id="outlined-size-small"
+                                placeholder='Insira senha'
+                                size="small"
+                                type="password"
+                                className='input'
+                            
+                            />
+                        </Grid>
+                        <Grid item>
+                            <Button variant="contained">Login</Button>
+                        </Grid>
                     </Grid>
-                    <Grid item xs={12}>
-                        <TextField
-                            label="CPF"
-                            id="outlined-size-small"
-                            placeholder="Insiea CPF"
-                            size="small"
-                        />
-
-                    </Grid>
-                    <Grid item xs={12}>
-                        <TextField
-                            label="Senha"
-                            id="outlined-size-small"
-                            placeholder='Insira senha'
-                            size="small"
-                            type="password"
-                        />
-
-                    </Grid>
-                    <Grid item>
-                        <Button variant="contained">Login</Button>
-
-                    </Grid>
-
-                </Grid>
-            </Card>
+                </Card>
+            </div>
         </Box>
 
     );
